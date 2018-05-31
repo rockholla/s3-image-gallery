b6ae852f20c7780aa11b4d62ce07c7fd400516a9
Patrick Force <patrickforce@gmail.com>
2018-05-31 17:59:44 -0600

    remove debug line

--- src/services/Api/index.js
+++ src/services/Api/index.js
@@ -2,7 +2,6 @@ import Mock from './Mock'
 import Aws from './Aws'
 
 const api = function () {
-  console.log(process.env)
   if (process.env.NODE_ENV === 'development') {
     return new Mock(process.env.VUE_APP_SECURITY_TYPE, process.env.VUE_APP_GLOBAL_AUTH_TOKEN)
   } else {
