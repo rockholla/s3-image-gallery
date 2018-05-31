19b46bd5618adda2452126877fe0ab81e23082c4
Patrick Force <patrickforce@gmail.com>
2018-05-31 16:27:34 -0600

    adding gzip build support, cleaning up dependencies, etc

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   devServer: {
     port: 8081
   },
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
+  }
 }
